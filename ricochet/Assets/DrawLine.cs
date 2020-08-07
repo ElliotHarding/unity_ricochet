cdf76059928b07a2d333dd0ebc6a89c03462c4cb
elliotharding <elliot.one.harding@gmail.com>
2020-08-07 18:51:52 +0100

    First commit

---
+++ ricochet/Assets/DrawLine.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+	protected LineRenderer m_LineRenderer;
+	protected EdgeCollider2D m_EdgeCollider2D;
+	protected Camera m_Camera;
+	protected List<Vector2> m_Points;
+    public Material m_drawnMaterial;
+
+	public virtual LineRenderer lineRenderer
+	{
+		get
+		{
+			return m_LineRenderer;
+		}
+	}
+
+	public virtual EdgeCollider2D edgeCollider2D
+	{
+		get
+		{
+			return m_EdgeCollider2D;
+		}
+	}
+
+	public virtual List<Vector2> points
+	{
+		get
+		{
+			return m_Points;
+		}
+	}
+
+	protected virtual void Awake ()
+	{
+		if ( m_LineRenderer == null )
+		{
+			Debug.LogWarning ( "DrawLine: Line Renderer not assigned, Adding and Using default Line Renderer." );
+			CreateDefaultLineRenderer ();
+		}
+		if ( m_EdgeCollider2D == null)
+		{
+			Debug.LogWarning ( "DrawLine: Edge Collider 2D not assigned, Adding and Using default Edge Collider 2D." );
+			CreateDefaultEdgeCollider2D ();
+		}
+		if ( m_Camera == null ) {
+			m_Camera = Camera.main;
+		}
+		m_Points = new List<Vector2> ();
+	}
+
+	protected virtual void Update ()
+	{
+		if ( Input.GetMouseButtonDown ( 1 ) )
+		{
+			Reset ();
+		}
+		if ( Input.GetMouseButton ( 0 ) )
+		{
+			Vector2 mousePosition = m_Camera.ScreenToWorldPoint ( Input.mousePosition );
+			if ( !m_Points.Contains ( mousePosition ) )
+			{
+				m_Points.Add ( mousePosition );
+				m_LineRenderer.positionCount = m_Points.Count;
+				m_LineRenderer.SetPosition ( m_LineRenderer.positionCount - 1, mousePosition );
+				if ( m_EdgeCollider2D != null && m_Points.Count > 1 )
+				{
+					m_EdgeCollider2D.points = m_Points.ToArray ();
+				}
+			}
+		}
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Up");
+        }
+	}
+
+	protected virtual void Reset ()
+	{
+		if ( m_LineRenderer != null )
+		{
+			m_LineRenderer.positionCount = 0;
+		}
+		if ( m_Points != null )
+		{
+			m_Points.Clear ();
+		}
+		if ( m_EdgeCollider2D != null)
+		{
+			m_EdgeCollider2D.Reset ();
+		}
+	}
+
+	protected virtual void CreateDefaultLineRenderer ()
+	{
+		m_LineRenderer = gameObject.AddComponent<LineRenderer> ();
+		m_LineRenderer.positionCount = 0;
+		m_LineRenderer.material = m_drawnMaterial;
+		m_LineRenderer.startColor = Color.white;
+		m_LineRenderer.endColor = Color.white;
+		m_LineRenderer.startWidth = 0.2f;
+		m_LineRenderer.endWidth = 0.2f;
+		m_LineRenderer.useWorldSpace = true;
+	}
+
+	protected virtual void CreateDefaultEdgeCollider2D ()
+	{
+		m_EdgeCollider2D = gameObject.AddComponent<EdgeCollider2D> ();
+	}
+
+}
---
+++ ricochet/Assets/LineDrawer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour
+{
+	public Camera m_camera;
+    public Material m_drawnMaterial;
+    public float m_cDrawWidth = 0.1f;
+
+    private bool m_bDrawing = false;
+    private LineRenderer m_drawingLineRenderer;
+    private GameObject m_drawingGameObject;
+    private Vector2 m_startDrawingPosition;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if ( Input.GetMouseButtonDown(0))
+		{
+			m_startDrawingPosition = m_camera.ScreenToWorldPoint ( Input.mousePosition );
+			m_bDrawing = true;
+
+            m_drawingGameObject = new GameObject();
+            m_drawingGameObject.transform.position = m_startDrawingPosition;
+            m_drawingGameObject.AddComponent<LineRenderer>();
+
+            m_drawingLineRenderer = m_drawingGameObject.GetComponent<LineRenderer>();
+            m_drawingLineRenderer.positionCount = 2;
+            m_drawingLineRenderer.material = m_drawnMaterial;
+            m_drawingLineRenderer.startColor = Color.white;
+            m_drawingLineRenderer.endColor = Color.white;
+            m_drawingLineRenderer.startWidth = m_cDrawWidth;
+            m_drawingLineRenderer.endWidth = m_cDrawWidth;
+            m_drawingLineRenderer.useWorldSpace = true;
+            m_drawingLineRenderer.SetPosition(0, m_startDrawingPosition);
+            m_drawingLineRenderer.SetPosition(1, m_startDrawingPosition);
+		}        
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_bDrawing = false;
+
+            List<Vector2> points = new List<Vector2>();
+            points.Add(m_drawingLineRenderer.GetPosition(0));
+            points.Add(m_drawingLineRenderer.GetPosition(1));
+
+            EdgeCollider2D edgeCollider2D = m_drawingGameObject.AddComponent<EdgeCollider2D>();
+            edgeCollider2D.points = points.ToArray();
+        }
+        else if (m_bDrawing)
+        {
+            Vector2 mousePosition = m_camera.ScreenToWorldPoint ( Input.mousePosition );
+            m_drawingLineRenderer.SetPosition(1, mousePosition);
+        }
+    }
+}
